d29e844472b10105d37f1260a8aa5e486281f3ac
enver-isliamov <enverphoto@gmail.com>
2025-08-26 20:51:22 +0300

    Update layout.tsx
    Signed-off-by: enver-isliamov <enverphoto@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -99,25 +99,23 @@ export default function RootLayout({
         <meta name="msapplication-tap-highlight" content="no" />
 
         {/* Яндекс.Метрика - ВСТАВЬТЕ СЮДА ВАШ КОД СЧЕТЧИКА */}
-        {/* 
-        Пример:
-        <script type="text/javascript">
-           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-           m[i].l=1*new Date();
-           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-           ym(ВАSH_COUNTER_ID, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
-           });
-        </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/ВАSH_COUNTER_ID" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-        */}
-        {/* Конец Яндекс.Метрики */}
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+    ym(97808515, 'init', {webvisor:true, clickmap:true, accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/97808515" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+
+
 
         {/* Structured Data */}
         <script
