bbfa7478d9fae58af9f5769974d447b4faaed9ee
enver-isliamov <enverphoto@gmail.com>
2025-10-11 00:43:03 +0300

    Update contact-cta.tsx
    Signed-off-by: enver-isliamov <enverphoto@gmail.com>

--- components/contact-cta.tsx
+++ components/contact-cta.tsx
@@ -72,7 +72,7 @@ export default function ContactCTA() {
                   <Clock className="w-5 h-5 mr-3 text-blue-600" />
                   <h4 className="font-semibold text-gray-900">Режим работы</h4>
                 </div>
-                <p className="text-gray-600 ml-8">Ежедневно с 9:00 до 22:00</p>
+                <p className="text-gray-600 ml-8">Ежедневно с 9:00 до 21:00</p>
               </div>
             </div>
 
